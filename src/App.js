dc8ca5ccace537a5740fab093e0516c36d30cd3a
Andrea N <andrea@narbot.com>
2024-03-19 08:40:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
+  const convertToPdf = () => {
+    window.print();
+  };
+
   return (
     <div className="App">
-      <button className="btn" onClick={() => window.print()}
-      >PDF converter
+      <button className="btn" onClick={convertToPdf}>
+        Convert to PDF
       </button>
+    
+      
     </div>
   );
 }
